16a6b25cc97fd74e10e057157f39c776a3d5e2fc
KanZhou <zhou.kan1@northeastern.edu>
2021-03-18 05:04:46 +0800

    ad

---
+++ Grokking/src/slidingWindow.java
@@ -0,0 +1,16 @@
+/*
+ * @Author: Kan Zhou
+ * @Email: zhou.kan1@northeastern.edu
+ * @Date: 2021-03-10 15:27:30
+ */
+
+import java.util.*;
+
+class AverageOfSubarrays{
+    public static double[] findAverage(int k, int[] arr) {
+        double[] res = new double[arr.length - k + 1];
+
+
+    }
+    
+}
