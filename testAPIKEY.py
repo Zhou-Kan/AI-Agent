e7f865184a3e1a2672aa62c1f2cfe2cb1e890ccd
kankanzhou <121850056+kankanzhou@users.noreply.github.com>
2026-02-25 08:55:28 -0800

    Update testAPIKEY to test OPENAI API

--- testAPIKEY.py
+++ testAPIKEY.py
@@ -1,1 +1,27 @@
+from openai import OpenAI
 
+client = OpenAI(
+    # If you haven't configured an environment variable, replace it with your Alibaba Cloud Bailian API Key：api_key="sk-xxx"
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+messages = [{"role": "user", "content": "who are you"}]
+completion = client.chat.completions.create(
+    model="qwen3.5-plus",  # model type
+    messages=messages,
+    extra_body={"enable_thinking": True},
+    stream=True
+)
+
+is_answering = False  # if you want to get answers
+print("\n" + "=" * 20 + "thought process" + "=" * 20)
+for chunk in completion:
+    delta = chunk.choices[0].delta
+    if hasattr(delta, "reasoning_content") and delta.reasoning_content is not None:
+        if not is_answering:
+            print(delta.reasoning_content, end="", flush=True)
+    if hasattr(delta, "content") and delta.content:
+        if not is_answering:
+            print("\n" + "=" * 20 + "full reply" + "=" * 20)
+            is_answering = True
+        print(delta.content, end="", flush=True)
